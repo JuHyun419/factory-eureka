90c97dc5506ad9946200f2f7eaa34639130af921
juhyun <zzang9ha@naver.com>
2023-09-23 10:57:06 +0900

    Initial commit

---
+++ src/test/kotlin/com/factory/factoryeureka/FactoryEurekaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.factory.factoryeureka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FactoryEurekaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
